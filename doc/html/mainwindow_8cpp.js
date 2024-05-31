96f8ab55d89e5b1ba68c7c91280525f75515a135
capoun17 <152632749+capoun17@users.noreply.github.com>
2024-05-31 19:38:20 +0200

    Add files via upload

---
+++ doc/html/mainwindow_8cpp.js
@@ -0,0 +1,4 @@
+var mainwindow_8cpp =
+[
+    [ "getRectangleVertices", "mainwindow_8cpp.html#a814c9577633f6e09d78a6a05e156c6dc", null ]
+];
---
+++ doc/html/structqt__meta__stringdata__MainWindow__t.js
@@ -0,0 +1,5 @@
+var structqt__meta__stringdata__MainWindow__t =
+[
+    [ "data", "structqt__meta__stringdata__MainWindow__t.html#afd67093eee085680898ad79aa678a6fe", null ],
+    [ "stringdata0", "structqt__meta__stringdata__MainWindow__t.html#a2e1c98c462fa55638604bf9fe7c566db", null ]
+];
